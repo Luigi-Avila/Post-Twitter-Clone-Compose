fb6acecf5604543cd57f9de009caf527d85d892a
Luigi-Avila <l.avila.uvg@gmail.com>
2023-01-30 13:37:28 -0600

    Challenge finished

--- app/src/main/java/com/example/post_twitter_clone_compose/MainActivity.kt
+++ app/src/main/java/com/example/post_twitter_clone_compose/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.post_twitter_clone_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.post_twitter_clone_compose.ui.theme.PostTwitterCloneComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +22,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    Column(Modifier.verticalScroll(rememberScrollState())) {
+                        PostItem()
+                        PostItem()
+                        PostItem()
+                    }
 
                 }
             }
--- app/src/main/java/com/example/post_twitter_clone_compose/TwitterPostScreen.kt
+++ app/src/main/java/com/example/post_twitter_clone_compose/TwitterPostScreen.kt
@@ -2,23 +2,37 @@ package com.example.post_twitter_clone_compose
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+
 
 @Preview(showSystemUi = true)
 @Composable
 fun PostPreview() {
+    PostItem()
+}
+
+@Composable
+fun PostItem(){
     Column(
         Modifier
             .fillMaxSize()
@@ -39,7 +53,11 @@ fun PostPreview() {
 
 @Composable
 fun PostFooter() {
-    Row() {
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(bottom = 15.dp), horizontalArrangement = Arrangement.SpaceAround
+    ) {
         ItemComment()
         ItemRT()
         ItemLike()
@@ -48,42 +66,105 @@ fun PostFooter() {
 
 @Composable
 fun ItemLike() {
-    Row() {
-        Icon(painter = painterResource(id = R.drawable.ic_like), contentDescription = "Like icon")
-        Text(text = "1")
+    var isActivated by rememberSaveable{ mutableStateOf(false) }
+    Row(modifier = Modifier.clickable { isActivated = !isActivated }) {
+        if (isActivated){
+            Icon(
+                painter = painterResource(id = R.drawable.ic_like_filled),
+                contentDescription = "Like icon Filled",
+                tint = Color.Red
+            )
+            Text(text = "2", color = Color.Gray)
+        } else {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_like),
+                contentDescription = "Like icon",
+                tint = Color.Gray
+            )
+            Text(text = "1", color = Color.Gray)
+        }
     }
 }
 
 @Composable
 fun ItemRT() {
-    Row() {
-        Icon(painter = painterResource(id = R.drawable.ic_rt), contentDescription = "Rt icon")
-        Text(text = "1")
+    var isActivated by rememberSaveable { mutableStateOf(false) }
+    Row(Modifier.clickable {
+        isActivated = !isActivated
+    }) {
+        if (isActivated){
+            Icon(
+                painter = painterResource(id = R.drawable.ic_rt),
+                contentDescription = "Rt icon",
+                tint = Color.Green
+            )
+            Text(text = "2", color = Color.Gray)
+        } else {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_rt),
+                contentDescription = "Rt icon",
+                tint = Color.Gray
+            )
+            Text(text = "1", color = Color.Gray)
+        }
+
+
     }
 }
 
 @Composable
 fun ItemComment() {
-    Row() {
-        Icon(
-            painter = painterResource(id = R.drawable.ic_chat),
-            contentDescription = "Comment icon"
-        )
-        Text(text = "1")
+    var isActivated by rememberSaveable { mutableStateOf(false) }
+    Row(Modifier.clickable {
+        isActivated = !isActivated
+    }) {
+        if (isActivated) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_chat_filled),
+                contentDescription = "Comment icon filled",
+                tint = Color.Gray
+            )
+            Text(text = "2", color = Color.Gray)
+        } else {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_chat),
+                contentDescription = "Comment icon",
+                tint = Color.Gray
+            )
+            Text(text = "1", color = Color.Gray)
+        }
+
     }
 }
 
 @Composable
 fun PostContent() {
     val text =
-        "t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'."
-    Column() {
+        "t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more or less normal distribution of letters, as opposed to using 'Content here, content here'."
+    Column {
         Text(text = text, modifier = Modifier.padding(end = 12.dp), color = Color.White)
         Image(
-            painter = painterResource(id = R.drawable.luigi_profile),
-            contentDescription = "Post content image",
-            modifier = Modifier.fillMaxWidth()
+            painter = painterResource(id = R.drawable.profile),
+            contentDescription = "Post image",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 15.dp)
+                .clip(RoundedCornerShape(50f))
+                .height(200.dp)
+                .align(Alignment.CenterHorizontally),
+            contentScale = ContentScale.Crop
         )
+        /*
+        Image(
+
+            painter = painterResource(id = R.drawable.profile),
+            contentDescription = "Post content image",
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.CenterHorizontally)
+                .height(150.dp)
+        )
+         */
     }
 }
 
